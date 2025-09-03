d3cfca3114fd4195e1378b38bb58093fc711ff19
Doythan <won03289@gmail.com>
2025-09-03 17:14:16 +0900

    MoDB ëH2´터 추가

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,19 @@
+SET REFERENTIAL_INTEGRITY FALSE;
+
+-- 자식 먼저
+TRUNCATE TABLE board_tb;
+
+-- 그 다음 부모들
+TRUNCATE TABLE category_tb;
+TRUNCATE TABLE user_tb;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+-- 샘플 데이터 (컬럼은 실제 스키마에 맞춰 조정)
+INSERT INTO user_tb (username, password, email, profile_image)
+VALUES ('u1', 'pw', 'u1@test.com', 'default.png');
+
+INSERT INTO category_tb (category, user_id)
+VALUES ('개발', 1), ('공지', 1);
+
+-- 필요하면 board_tb INSERT도 추가
