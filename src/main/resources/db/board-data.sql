9c46ca44a4d6f00b775e3b65d5815c975f05a68b
verynahce <a32176740@gmail.com>
2025-09-05 13:28:00 +0900

    [feat] 데이터 정리

--- src/main/resources/db/board-data.sql
+++ src/main/resources/db/board-data.sql
@@ -1,6 +1,9 @@
-insert into board_tb(title, content, userId, views, categoryId, createdAt, updatedAt)
-values ('제목1', '내용1', 1,1,1, DATE_SUB(NOW(), INTERVAL 1 HOUR), now()),
-       ('제목2', '내용2', 2,10,2, NOW(), null),
-       ('제목3', '내용3', 3,100,3, DATE_SUB(NOW(), INTERVAL 1 HOUR), now()),
-       ('제목4', '내용4', 1,200,1, NOW(), null),
-       ('제목5', '내용5', 2,300,2, DATE_SUB(NOW(), INTERVAL 1 HOUR), now());
+INSERT INTO board_tb(title, content, user_id, views, category_id, created_at, updated_at)
+VALUES
+    ('제목1', '내용1', 1, 1, 1, CURRENT_TIMESTAMP - INTERVAL '1' HOUR, CURRENT_TIMESTAMP),
+    ('제목2', '내용2', 2, 10, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('제목3', '내용3', 3, 100, 3, CURRENT_TIMESTAMP - INTERVAL '1' HOUR, CURRENT_TIMESTAMP),
+    ('제목4', '내용4', 1, 200, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('제목5', '내용5', 2, 300, 2, CURRENT_TIMESTAMP - INTERVAL '1' HOUR, CURRENT_TIMESTAMP);
+
+
