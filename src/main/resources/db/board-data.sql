51e113c749a58437fed2730c6f7f4d92e282c6a5
Sxias <a32176740@gmail.com>
2025-09-05 14:36:45 +0900

    board data 수정

--- src/main/resources/db/board-data.sql
+++ src/main/resources/db/board-data.sql
@@ -1,6 +1,7 @@
-insert into board_tb(title, content, user_id, views, category_id, created_at, updated_at)
-values ('제목1', '내용1', 1,1,1, now(), now()),
-       ('제목2', '내용2', 2,10,2, NOW(), null),
-       ('제목3', '내용3', 3,100,3, now(), now()),
-       ('제목4', '내용4', 1,200,1, NOW(), null),
-       ('제목5', '내용5', 2,300,2, now(), now());
+INSERT INTO board_tb(title, content, user_id, views, category_id, created_at, updated_at)
+VALUES
+    ('제목1', '내용1', 1, 1, 1, CURRENT_TIMESTAMP - INTERVAL '1' HOUR, CURRENT_TIMESTAMP),
+    ('제목2', '내용2', 2, 10, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('제목3', '내용3', 3, 100, 3, CURRENT_TIMESTAMP - INTERVAL '1' HOUR, CURRENT_TIMESTAMP),
+    ('제목4', '내용4', 1, 200, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('제목5', '내용5', 2, 300, 2, CURRENT_TIMESTAMP - INTERVAL '1' HOUR, CURRENT_TIMESTAMP);
